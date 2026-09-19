27a2e29e2cfd04836e327018f6e0f8c7c566169d
SantiagoAte789 <jaramillo.atehortua01@uceva.edu.co>
2026-09-18 21:17:46 -0500

    feat: agregar declaración de tipos básicos con typeof

---
+++ Corte II/TypeScript/Tasks/ejercicio_1.ts
@@ -0,0 +1,11 @@
+const nombre: string = "Laura";
+const edad: number = 21;
+const salario: number = 3500;
+const estaActivo: boolean = true;
+const ciudad: string = "Bogotá";
+
+console.log(nombre, typeof nombre);
+console.log(edad, typeof edad);
+console.log(salario, typeof salario);
+console.log(estaActivo, typeof estaActivo);
+console.log(ciudad, typeof ciudad);
