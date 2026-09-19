0549133729c9aa6dfaea6c40bce42c9374c1cfe5
SantiagoAte789 <jaramillo.atehortua01@uceva.edu.co>
2026-09-18 20:05:23 -0500

    feat: agregar función tablaMultiplicar para imprimir tabla de multiplicar

---
+++ Corte II/JavaScript/Tasks/ejercicio_2.js
@@ -0,0 +1,8 @@
+function tablaMultiplicar(numero, cantidad) {
+    for (let i = 1; i <= cantidad; i++) {
+        console.log(numero + " x " + i + " = " + (numero * i));
+    }
+}
+
+
+tablaMultiplicar(2, 15);
